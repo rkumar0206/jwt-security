065353987b41fcece4e2d3a1131d759aaa35d7a6
Rohit Kumar <rkumar8092378845@gmail.com>
2026-08-04 00:23:39 +0530

    feat: enabled method security

--- lib/src/main/java/com/rksdev/security/config/SecurityConfig.java
+++ lib/src/main/java/com/rksdev/security/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ import java.util.Collections;
 
 @AutoConfiguration
 @EnableWebSecurity
+@EnableMethodSecurity
 @EnableConfigurationProperties(JwtProperties.class)
 @ComponentScan(basePackages = "com.rksdev.security")
 @ConditionalOnProperty(prefix = "jwt.properties", name = "enabled", havingValue = "true", matchIfMissing = true)
